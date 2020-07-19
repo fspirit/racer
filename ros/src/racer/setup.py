a2f3fc22388daba4863b5ea1f7bcd61ba76f405f
stas.olekhnovich <stas.olekhnovich@gmail.com>
2020-07-19 23:56:15 +0200

    Make setup copy files from all py sources to build output dir

--- ros/src/racer/setup.py
+++ ros/src/racer/setup.py
@@ -1,18 +1,18 @@
 import os
 from setuptools import setup
 from glob import glob
+from setuptools import find_packages
 
 package_name = 'racer'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
+        ('share/' + package_name, ['package.xml']),        
         (os.path.join('share', package_name, 'launch'), glob('*.launch.py'))
     ],
     install_requires=['setuptools'],
@@ -23,8 +23,10 @@ setup(
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
-        'console_scripts': ['servo_driver = racer.servo_driver.main:main',
-            'control_error_extractor = racer.control_error_extractor.main:main'
+        'console_scripts': 
+        [
+            'servo_driver = racer.servo_driver.main:main',
+            'control_error_extractor = racer.control_error_extractor.main:main'            
         ],
     },
 )
